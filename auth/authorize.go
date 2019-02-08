17d50ff01d98f9107ef81a8f9d5f03f1453252a5
Andres Colonia <andreskal@gmail.com>
2019-02-08 14:59:27 -0300

    rename function

--- auth/authorize.go
+++ auth/authorize.go
@@ -8,7 +8,7 @@ import (
 
 const Secret = "ee71a17fa8b84f99c3b99bdedb3b6e6910cfd59ed3fb0057ba96f72d2e952e0c4a26d0509702f2772ae20f8cc652124c88df5fb9a9b265ac12ee1af83e9ef0ba"
 
-func Auth(tokenString string) (*jwt.Token, error) {
+func Sign(tokenString string) (*jwt.Token, error) {
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
